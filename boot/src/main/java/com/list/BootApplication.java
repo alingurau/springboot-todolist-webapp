34904a1770ff3d0a70ce05edb267d22c3f590916
Alin Gurau <alin.gurau@emanuel.ro>
2018-11-15 17:11:11 +0200

    refactoring

--- boot/src/main/java/com/list/BootApplication.java
+++ boot/src/main/java/com/list/BootApplication.java
@@ -2,8 +2,12 @@ package com.list;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class BootApplication {
 
     public static void main(String[] args) {
